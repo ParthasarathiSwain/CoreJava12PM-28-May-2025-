0ef4ad3051a09c66d1a235155bd068f8c1580597
ParthasarathiSwain <rajaswain6969@gmail.com>
2025-06-03 13:40:17 +0530

    Uploading 03-06-2025 Notes

---
+++ operator/Test02.java
@@ -0,0 +1,12 @@
+class Test02{
+	public static void main(String args[]){
+		int r = 10 - 20 / 5 * 4 + 3 - 2 * 10 % 5  ;
+		System.out.println(r);
+
+
+		int x=23;
+		int y=  x++   +   ++x    +    ++x    +  x++ ; 
+		System.out.println("Y : "+y);
+ 
+	}
+}
