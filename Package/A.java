f9eb1b1cb144848cf7b9fae4f530ea1e5600fdf3
ParthasarathiSwain <rajaswain6969@gmail.com>
2025-06-19 19:01:21 +0530

    Uploading 19-06-2025 Notes

---
+++ Package/A.java
@@ -0,0 +1,9 @@
+package p1;
+
+class A{
+	public static void main(String ar[]){
+		System.out.println("Testing...");
+		System.out.println(10/0);
+
+	}
+}
---
+++ cmdline arg/Test.java
@@ -0,0 +1,22 @@
+class  Test{
+	public static void main(String args[]){
+	/*	System.out.println(args[0])		  	
+		System.out.println(args[1]); //java.lang.ArrayIndexOutOfBoundsException
+
+		String a=args[0];
+		String b=args[1];
+		System.out.println(a+b);//1020
+
+		String a=args[0];
+		int x=Integer.parseInt(a);
+
+		String b=args[1];
+		int y=Integer.parseInt(b);
+
+		System.out.println(x+y);
+	*/
+		int x=Integer.parseInt(args[0]);
+		int y=Integer.parseInt(args[1]);
+		System.out.println(x+y);
+	}
+}
